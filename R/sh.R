037e3e10d644f5fc7ec6177273ee08188cd25d70
sudeepthi5 <97864497+sudeepthi5@users.noreply.github.com>
2022-05-02 04:50:32 -0400

    Add files via upload

---
+++ R/sh.R
@@ -0,0 +1,22 @@
+#' create graphs and numerical data
+#' @export
+#' @param x numerical variable
+
+sh <- function(x)
+{
+  # 1 row 2 columns
+  par(mfrow = c(1,2))
+
+  #Graphical summary
+  #Histogram
+  hist(x, col = rainbow(20))
+
+  #Numerical summary
+  data.frame(mean= mean(x),
+             median= median(x),
+             sum=sum(x),
+             sqrt= sqrt(x),
+             trunc= trunc(x),
+             skewness=skewness(x),
+             kurtosis= kurtosis(x))
+}
